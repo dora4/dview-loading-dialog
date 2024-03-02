2d658d13317c2912844db853659935f648c626b0
dora <dora924666990@gmail.com>
2024-03-02 20:13:06 +0800

    新增多国语言翻译

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
 
@@ -36,7 +36,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = rootProject.project.name
-                version = "1.4"
+                version = "1.5"
             }
         }
     }
